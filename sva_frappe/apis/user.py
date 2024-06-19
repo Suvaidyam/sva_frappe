7c2902de5275eb5d387cdd2a60f146a07aff11ad
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2024-06-19 13:27:46 +0530

    zone by permission handle

--- sva_frappe/apis/user.py
+++ sva_frappe/apis/user.py
@@ -2,11 +2,14 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def get_user_permission(user, join_con=[]):
+    # is_zone_mandatory = frappe.db.get_single_value('User Settings','is_zone_mandatory')
     sql_query = f"""
         SELECT
             CASE
+                WHEN UP.allow = 'Zone' THEN ZN.zone_name
                 WHEN UP.allow = 'State' THEN TS.state_name
                 WHEN UP.allow = 'District' THEN TD.district_name
+                WHEN UP.allow = 'Center Location' THEN CL.center_location_name
                 WHEN UP.allow = 'Block' THEN TB.block_name
                 WHEN UP.allow = 'Village' THEN TCS.village_name
             END AS name_value,
@@ -15,10 +18,12 @@ def get_user_permission(user, join_con=[]):
             UP.allow,
             UP.user
         FROM `tabUser Permission` AS UP
-        LEFT JOIN `tabState` AS TS ON UP.for_value = TS.name AND UP.allow = 'state'
-        LEFT JOIN `tabDistrict` AS TD ON UP.for_value = TD.name AND UP.allow = 'district'
-        LEFT JOIN `tabBlock` AS TB ON UP.for_value = TB.name AND UP.allow = 'block'
-        LEFT JOIN `tabVillage` AS TCS ON UP.for_value = TCS.name AND UP.allow = 'village'
+        LEFT JOIN `tabZone` AS ZN ON UP.for_value = ZN.name AND UP.allow = 'Zone'
+        LEFT JOIN `tabState` AS TS ON UP.for_value = TS.name AND UP.allow = 'State'
+        LEFT JOIN `tabDistrict` AS TD ON UP.for_value = TD.name AND UP.allow = 'District'
+        LEFT JOIN `tabCenter Location` AS CL ON UP.for_value = CL.name AND UP.allow = 'Center Location'
+        LEFT JOIN `tabBlock` AS TB ON UP.for_value = TB.name AND UP.allow = 'Block'
+        LEFT JOIN `tabVillage` AS TCS ON UP.for_value = TCS.name AND UP.allow = 'Village'
         WHERE UP.user = '{user}'
     """
     return frappe.db.sql(sql_query, as_dict=True)
--- sva_frappe/apis/user_permissions.py
+++ sva_frappe/apis/user_permissions.py
@@ -8,10 +8,31 @@ def create_user_permissions(doc=None):
     for_value = document.get('for_value')
     doctype = document.get('doctype')
     user = document.get('user')
-    
-    if allow == "State":
-        exist = frappe.db.exists("User Permission", {"allow": "State", "for_value": for_value, "user": user})
+    is_zone_mandatory = frappe.db.get_single_value('User Settings','is_zone_mandatory')
+    if allow =="zone":
+        exist = frappe.db.exists("User Permission", {"allow": "Zone", "for_value": for_value, "user": user})
         if not exist:
+            zone_perm = frappe.get_doc({
+                'doctype': 'User Permission',
+                'allow': 'Zone',
+                'for_value': for_value,
+                'user': user
+            })
+            zone_perm.insert(ignore_permissions=True)
+    elif allow == "State":
+        state_exist = frappe.db.exists("User Permission", {"allow": "State", "for_value": for_value, "user": user})
+        if is_zone_mandatory:
+            zone_value = frappe.db.get_value("State", for_value, "zone")
+            zone_exist = frappe.db.exists("User Permission", {"allow": "Zone", "for_value": zone_value, "user": user})
+            if not zone_exist:
+                zone_perm = frappe.get_doc({
+                    'doctype': 'User Permission',
+                    'allow': 'Zone',
+                    'for_value': zone_value,
+                    'user': user
+                })
+                zone_perm.insert(ignore_permissions=True)
+        if not state_exist:
             state_perm = frappe.get_doc({
                 'doctype': 'User Permission',
                 'allow': 'State',
@@ -22,6 +43,17 @@ def create_user_permissions(doc=None):
     elif allow == "District":
         state_value = frappe.db.get_value("District", for_value, "state")
         if state_value:
+            if is_zone_mandatory:
+                zone_value = frappe.db.get_value("State", state_value, "zone")
+                zone_exist = frappe.db.exists("User Permission", {"allow": "Zone", "for_value": zone_value, "user": user})
+                if not zone_exist:
+                    zone_perm = frappe.get_doc({
+                        'doctype': 'User Permission',
+                        'allow': 'Zone',
+                        'for_value': zone_value,
+                        'user': user
+                    })
+                    zone_perm.insert(ignore_permissions=True)
             state_exist = frappe.db.exists("User Permission", {"allow": "State", "for_value": state_value, "user": user})
             dist_exist = frappe.db.exists("User Permission", {"allow": "District", "for_value": for_value, "user": user})
             if not state_exist:
@@ -40,10 +72,63 @@ def create_user_permissions(doc=None):
                     'user': user
                 })
                 dist_perm.insert(ignore_permissions=True)
+    elif allow == "Center Location":
+        state_value = frappe.db.get_value("Center Location", for_value, "state")
+        dist_value = frappe.db.get_value("Center Location", for_value, "district")
+        if state_value and dist_value:
+            if is_zone_mandatory:
+                zone_value = frappe.db.get_value("State", state_value, "zone")
+                zone_exist = frappe.db.exists("User Permission", {"allow": "Zone", "for_value": zone_value, "user": user})
+                if not zone_exist:
+                    zone_perm = frappe.get_doc({
+                        'doctype': 'User Permission',
+                        'allow': 'Zone',
+                        'for_value': zone_value,
+                        'user': user
+                    })
+                    zone_perm.insert(ignore_permissions=True)
+            state_exist = frappe.db.exists("User Permission", {"allow": "State", "for_value": state_value, "user": user})
+            dist_exist = frappe.db.exists("User Permission", {"allow": "District", "for_value": dist_value, "user": user})
+            block_exist = frappe.db.exists("User Permission", {"allow": "Center Location", "for_value": for_value, "user": user})
+            if not state_exist:
+                state_perm = frappe.get_doc({
+                    'doctype': 'User Permission',
+                    'allow': 'State',
+                    'for_value': state_value,
+                    'user': user
+                })
+                state_perm.insert(ignore_permissions=True)
+            if not dist_exist:
+                dist_perm = frappe.get_doc({
+                    'doctype': 'User Permission',
+                    'allow': 'District',
+                    'for_value': dist_value,
+                    'user': user
+                })
+                dist_perm.insert(ignore_permissions=True)
+            if not block_exist:
+                block_perm = frappe.get_doc({
+                    'doctype': 'User Permission',
+                    'allow': 'Center Location',
+                    'for_value': for_value,
+                    'user': user
+                })
+                block_perm.insert(ignore_permissions=True)
     elif allow == "Block":
         state_value = frappe.db.get_value("Block", for_value, "state")
         dist_value = frappe.db.get_value("Block", for_value, "district")
         if state_value and dist_value:
+            if is_zone_mandatory:
+                zone_value = frappe.db.get_value("State", state_value, "zone")
+                zone_exist = frappe.db.exists("User Permission", {"allow": "Zone", "for_value": zone_value, "user": user})
+                if not zone_exist:
+                    zone_perm = frappe.get_doc({
+                        'doctype': 'User Permission',
+                        'allow': 'Zone',
+                        'for_value': zone_value,
+                        'user': user
+                    })
+                    zone_perm.insert(ignore_permissions=True)
             state_exist = frappe.db.exists("User Permission", {"allow": "State", "for_value": state_value, "user": user})
             dist_exist = frappe.db.exists("User Permission", {"allow": "District", "for_value": dist_value, "user": user})
             block_exist = frappe.db.exists("User Permission", {"allow": "Block", "for_value": for_value, "user": user})
@@ -76,6 +161,17 @@ def create_user_permissions(doc=None):
         dist_value = frappe.db.get_value("Grampanchayat", for_value, "district")
         block_value = frappe.db.get_value("Grampanchayat", for_value, "block")
         if state_value and dist_value and block_value:
+            if is_zone_mandatory:
+                zone_value = frappe.db.get_value("State", state_value, "zone")
+                zone_exist = frappe.db.exists("User Permission", {"allow": "Zone", "for_value": zone_value, "user": user})
+                if not zone_exist:
+                    zone_perm = frappe.get_doc({
+                        'doctype': 'User Permission',
+                        'allow': 'Zone',
+                        'for_value': zone_value,
+                        'user': user
+                    })
+                    zone_perm.insert(ignore_permissions=True)
             state_exist = frappe.db.exists("User Permission", {"allow": "State", "for_value": state_value, "user": user})
             dist_exist = frappe.db.exists("User Permission", {"allow": "District", "for_value": dist_value, "user": user})
             block_exist = frappe.db.exists("User Permission", {"allow": "Block", "for_value": block_value, "user": user})
