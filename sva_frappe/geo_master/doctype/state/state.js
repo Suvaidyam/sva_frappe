d476dae716281c59f5036ea3e0953f6ab2c1db6d
Samar Raj <samar.suvaidyam@gmail.com>
2024-11-10 10:32:23 +0000

    modified connections

--- sva_frappe/geo_master/doctype/state/state.js
+++ sva_frappe/geo_master/doctype/state/state.js
@@ -27,7 +27,6 @@ frappe.ui.form.on("State", {
         tabContent(frm, tab_field)
         $('a[data-toggle="tab"]').on('shown.bs.tab', async function (e) {
             let tab_field = frm.get_active_tab()?.df?.fieldname;
-            console.log(tab_field, 'tab_field')
             tabContent(frm, tab_field)
         });
         // role by permission
