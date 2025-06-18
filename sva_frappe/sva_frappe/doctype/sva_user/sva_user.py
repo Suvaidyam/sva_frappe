746aac1f57f244d2de4841b40b04bd299e63304f
amreshyadavgit <ami92.raj@gmail.com>
2025-06-18 20:25:33 +0000

    handled for username duplication, replaced email if username is empty

--- sva_frappe/sva_frappe/doctype/sva_user/sva_user.py
+++ sva_frappe/sva_frappe/doctype/sva_user/sva_user.py
@@ -51,8 +51,8 @@ class SVAUser(Document):
 
 		# Find and delete unallocated permissions
 		unallocated_permissions = frappe.get_list(
-			"User Permission", 
-			filters={'name': ['NOT IN', up_list], 'user': self.email}, 
+			"User Permission",
+			filters={'name': ['NOT IN', up_list], 'user': self.email},
 			pluck='name',
 			ignore_permissions=True
 		)
@@ -75,7 +75,7 @@ class SVAUser(Document):
 		new_user.first_name = self.first_name
 		new_user.middle_name = self.middle_name
 		new_user.last_name = self.last_name
-		new_user.username = self.username
+		new_user.username = self.username if self.username else self.email
 		new_user.mobile_no = self.mobile_number
 		new_user.role_profile_name = self.role_profile
 		new_user.user_image = self.user_image
@@ -90,7 +90,7 @@ class SVAUser(Document):
 			user_doc.first_name = self.first_name
 			user_doc.middle_name = self.middle_name
 			user_doc.last_name = self.last_name
-			user_doc.username = self.username
+			user_doc.username = self.username if self.username else self.email
 			user_doc.mobile_no = self.mobile_number
 			user_doc.user_image = self.user_image
 			user_doc.new_password = self.confirm_password
